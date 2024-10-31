53e7389a6421f5b7a2e257a1e85bc0e16260a0eb
Jay Haase <jay@clockwork.com>
2024-10-31 16:24:49 -0500

    Fixing structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function (eleventyConfig) {
   // Static assets
   eleventyConfig.addPassthroughCopy({ "src/assets/images": "images" });
-  eleventyConfig.addPassthroughCopy({ "src/assets/css": "css" });
   eleventyConfig.addPassthroughCopy({ "src/assets/js": "js" });
 
   return {
